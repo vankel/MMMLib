f0c5f6e8707610f2dd8f726898e07fe076d97adf
is04 <adresskari@gmail.com>
2020-07-16 18:11:22 +0900

    1.4.6v2

--- SRC-Modloader/MMM_ModelBiped.java
+++ SRC-Modloader/MMM_ModelBiped.java
@@ -1,6 +1,7 @@
 package net.minecraft.src;
 
 import java.util.Map;
+import java.util.Random;
 import java.util.Map.Entry;
 
 import javax.crypto.spec.PSource;
@@ -157,4 +158,59 @@ public abstract class MMM_ModelBiped extends ModelBiped {
     	return true;
     }
 	
+
+    // MathHelperƒgƒ“ƒlƒ‹ŠÖ”ŒQ
+    public static final float mh_sin(float f) {
+    	f = f % 6.283185307179586F;
+    	f = (f < 0F) ? 360 + f : f;
+        return MathHelper.sin(f);
+    }
+
+    public static final float mh_cos(float f) {
+    	f = f % 6.283185307179586F;
+    	f = (f < 0F) ? 360 + f : f;
+        return MathHelper.cos(f);
+    }
+
+    public static final float mh_sqrt_float(float f) {
+        return MathHelper.sqrt_float(f);
+    }
+
+    public static final float mh_sqrt_double(double d) {
+        return MathHelper.sqrt_double(d);
+    }
+
+    public static final int mh_floor_float(float f) {
+    	return MathHelper.floor_float(f);
+    }
+
+    public static final int mh_floor_double(double d) {
+        return MathHelper.floor_double(d);
+    }
+
+    public static final long mh_floor_double_long(double d) {
+        return MathHelper.floor_double_long(d);
+    }
+
+    public static final float mh_abs(float f) {
+    	return MathHelper.abs(f);
+    }
+
+    public static final double mh_abs_max(double d, double d1) {
+        return MathHelper.abs_max(d, d1);
+    }
+
+    public static final int mh_bucketInt(int i, int j) {
+        return MathHelper.bucketInt(i, j);
+    }
+
+    public static final boolean mh_stringNullOrLengthZero(String s) {
+        return MathHelper.stringNullOrLengthZero(s);
+    }
+
+    public static final int mh_getRandomIntegerInRange(Random random, int i, int j) {
+        return MathHelper.getRandomIntegerInRange(random, i, j);
+    }
+
+
 }
